6c3558b4d7c56956d4c7f336836f24e4e11dfe7d
liaobin <liaobin@thoughtworks.com>
2020-03-02 16:00:44 +0800

    refactor: add increaseQuality & reduceQuality method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -18,30 +18,10 @@ class GildedRose {
     }
 
     private void calculateQuality(Item item) {
-        if (!item.getName().equals(AGED_BRIE)
-                && !item.getName().equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-            if (item.getQuality() > 0) {
-                if (!item.getName().equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    item.setQuality(item.getQuality() - 1);
-                }
-            }
+        if (isSpecifyCommodity(item)) {
+            increaseQuality(item);
         } else {
-            if (item.getQuality() < MAX_QUALITY) {
-                item.setQuality(item.getQuality() + 1);
-                if (item.getName().equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                    if (item.getSellIn() < 11) {
-                        if (item.getQuality() < MAX_QUALITY) {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-
-                    if (item.getSellIn() < 6) {
-                        if (item.getQuality() < MAX_QUALITY) {
-                            item.setQuality(item.getQuality() + 1);
-                        }
-                    }
-                }
-            }
+            reduceQuality(item);
         }
 
         if (!item.getName().equals(SULFURAS_HAND_OF_RAGNAROS)) {
@@ -60,10 +40,44 @@ class GildedRose {
                     item.setQuality(0);
                 }
             } else {
-                if (item.getQuality() < MAX_QUALITY) {
+                if (isLessThanMaxQuality(item.getQuality())) {
                     item.setQuality(item.getQuality() + 1);
                 }
             }
         }
+    }
+
+    private void increaseQuality(Item item) {
+        if (isLessThanMaxQuality(item.getQuality())) {
+            item.setQuality(item.getQuality() + 1);
+            if (item.getName().equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                if (item.getSellIn() < 11) {
+                    if (isLessThanMaxQuality(item.getQuality())) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
+
+                if (item.getSellIn() < 6) {
+                    if (isLessThanMaxQuality(item.getQuality())) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    private void reduceQuality(Item item) {
+        if (item.getQuality() > 0 && !item.getName().equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            item.setQuality(item.getQuality() - 1);
+        }
+    }
+
+    private boolean isLessThanMaxQuality(int quality) {
+        return quality < MAX_QUALITY;
+    }
+
+    private boolean isSpecifyCommodity(Item item) {
+        return item.getName().equals(AGED_BRIE)
+                && item.getName().equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT);
     }
 }
