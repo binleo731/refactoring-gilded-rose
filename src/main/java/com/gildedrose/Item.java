093ecc30c2bb3b52326518196fe072fd87a34373
liaobin <liaobin@thoughtworks.com>
2020-03-02 15:34:55 +0800

    refactor: add lombok

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,18 +1,16 @@
 package com.gildedrose;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
-
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+    private String name;
+    private int sellIn;
+    private int quality;
 
     @Override
     public String toString() {
